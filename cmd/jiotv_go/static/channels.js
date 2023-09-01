d70a69cd6a2ef79d04eea256f5256a9fa845a30d
Mohammed Rabil <rabil@techie.com>
2023-09-01 20:55:04 +0530

    `channels.js`: Dump commented lines

--- cmd/jiotv_go/static/channels.js
+++ cmd/jiotv_go/static/channels.js
@@ -1,39 +1,3 @@
-// const languageOptions = [
-//     { value: 1, label: "Hindi" },
-//     { value: 2, label: "Marathi" },
-//     { value: 3, label: "Punjabi" },
-//     { value: 4, label: "Urdu" },
-//     { value: 5, label: "Bengali" },
-//     { value: 6, label: "English" },
-//     { value: 7, label: "Malayalam" },
-//     { value: 8, label: "Tamil" },
-//     { value: 9, label: "Gujarati" },
-//     { value: 10, label: "Odia" },
-//     { value: 11, label: "Telugu" },
-//     { value: 12, label: "Bhojpuri" },
-//     { value: 13, label: "Kannada" },
-//     { value: 14, label: "Assamese" },
-//     { value: 15, label: "Nepali" },
-//     { value: 16, label: "French" },
-//     { value: 18, label: "Other" }
-// ];
-
-// const categoryOptions = [
-//     { value: 5, label: "Entertainment" },
-//     { value: 6, label: "Movies" },
-//     { value: 7, label: "Kids" },
-//     { value: 8, label: "Sports" },
-//     { value: 9, label: "Lifestyle" },
-//     { value: 10, label: "Infotainment" },
-//     { value: 12, label: "News" },
-//     { value: 13, label: "Music" },
-//     { value: 15, label: "Devotional" },
-//     { value: 16, label: "Business" },
-//     { value: 17, label: "Educational" },
-//     { value: 18, label: "Shopping" },
-//     { value: 19, label: "JioDarshan" }
-// ];
-
 const languageElement = document.getElementById("portexe-language-select");
 const categoryElement = document.getElementById("portexe-category-select");
 const catLangApplyButton = document.getElementById("portexe-search-button");
