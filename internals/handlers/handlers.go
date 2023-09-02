9f9ec2d76f883753998aed9547d3e4fd0d0d8d44
Mohammed Rabil <rabil@techie.com>
2023-09-02 09:26:27 +0530

    Add `.m3u8` suffix for m3u playlist url

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -198,7 +198,7 @@ func ChannelsHandler(c *gin.Context) {
 		m3uContent := "#EXTM3U\n"
 		logoURL := "https://jiotv.catchup.cdn.jio.com/dare_images/images"
 		for _, channel := range apiResponse.Result {
-			channelURL := fmt.Sprintf("%s/live/%d", hostURL, channel.ID)
+			channelURL := fmt.Sprintf("%s/live/%d.m3u8", hostURL, channel.ID)
 			channelLOGOURL := fmt.Sprintf("%s/%s", logoURL, channel.LogoURL)
 			m3uContent += fmt.Sprintf("#EXTINF:-1 tvg-name=\"%s\" tvg-logo=\"%s\" tvg-language=\"%s\" tvg-type=\"%s\" group-title=\"%s\", %s\n%s\n",
 				channel.Name, channelLOGOURL, television.LanguageMap[channel.Language], television.CategoryMap[channel.Category], television.CategoryMap[channel.Category], channel.Name, channelURL)
