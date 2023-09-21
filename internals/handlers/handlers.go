7ed94268d0704ccb18528d541d797a1d6ae9d0e2
Mohammed Rabil <rabil@techie.com>
2023-09-22 01:50:42 +0530

    Use `%q` instead of `"%s"`

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -177,7 +177,7 @@ func ChannelsHandler(c *fiber.Ctx) error {
 		for _, channel := range apiResponse.Result {
 			channelURL := fmt.Sprintf("%s/live/%d.m3u8", hostURL, channel.ID)
 			channelLogoURL := fmt.Sprintf("%s/%s", logoURL, channel.LogoURL)
-			m3uContent += fmt.Sprintf("#EXTINF:-1 tvg-name=\"%s\" tvg-logo=\"%s\" tvg-language=\"%s\" tvg-type=\"%s\" group-title=\"%s\", %s\n%s\n",
+			m3uContent += fmt.Sprintf("#EXTINF:-1 tvg-name=%q tvg-logo=%q tvg-language=%q tvg-type=%q group-title=%q, %s\n%s\n",
 				channel.Name, channelLogoURL, television.LanguageMap[channel.Language], television.CategoryMap[channel.Category], television.CategoryMap[channel.Category], channel.Name, channelURL)
 		}
 
