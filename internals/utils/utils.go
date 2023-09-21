0f54cce2e9b87d8cab317169dc4f84a6a9b72845
Mohammed Rabil <rabil@techie.com>
2023-09-21 21:32:14 +0530

    Remove net/http custom dialer

--- internals/utils/utils.go
+++ internals/utils/utils.go
@@ -376,34 +376,3 @@ func CheckLoggedIn() bool {
 		return true
 	}
 }
-
-func GetCustomDialer() func(ctx context.Context, network string, addr string) (net.Conn, error) {
-	USER_CUSTOM_DNS := os.Getenv("JIOTV_DNS")
-	if USER_CUSTOM_DNS == "" {
-		USER_CUSTOM_DNS = "1.1.1.1"
-	}
-
-	var (
-		dnsResolverIP        = USER_CUSTOM_DNS + ":53" // Cloudflare DNS resolver.
-		dnsResolverProto     = "udp"                   // Protocol to use for the DNS resolver
-		dnsResolverTimeoutMs = 5000                    // Timeout (ms) for the DNS resolver (optional)
-	)
-
-	dialer := &net.Dialer{
-		Resolver: &net.Resolver{
-			PreferGo: true,
-			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-				d := net.Dialer{
-					Timeout: time.Duration(dnsResolverTimeoutMs) * time.Millisecond,
-				}
-				return d.DialContext(ctx, dnsResolverProto, dnsResolverIP)
-			},
-		},
-	}
-
-	dialContext := func(ctx context.Context, network, addr string) (net.Conn, error) {
-		return dialer.DialContext(ctx, network, addr)
-	}
-
-	return dialContext
-}
