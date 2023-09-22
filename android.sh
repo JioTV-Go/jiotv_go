8797996d021699d380e8c3100c647a9e6240da42
Mohammed Rabil <rabil@techie.com>
2023-09-23 00:06:47 +0530

    deepsource: ignore bash SH-2012

--- android.sh
+++ android.sh
@@ -89,7 +89,7 @@ install_android() {
 # Function to update the binary
 update_android() {
     # fetch existing file name, if multiple files are present, pick the latest one
-    existing_file_name=$(ls "$BINARY_NAME-*-$ARCH" | sort -r | head -n 1)
+    existing_file_name=$(ls "$BINARY_NAME-*-$ARCH" | sort -r | head -n 1) # skipcq: SH-2012
 
     if [ ! -z "$existing_file_name" ]; then
         echo "Found existing file: $existing_file_name"
@@ -124,7 +124,7 @@ update_android() {
 # Function to run the binary
 run_android() {
     # fetch file name from ls command, if multiple files are present, pick the latest one
-    file_name=$(ls "$BINARY_NAME-*-$ARCH" | sort -r | head -n 1)
+    file_name=$(ls "$BINARY_NAME-*-$ARCH" | sort -r | head -n 1) # skipcq: SH-2012
 
     # Check if the binary exists
     if [ -z "$file_name" ]; then
