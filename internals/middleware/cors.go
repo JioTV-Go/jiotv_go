560e06521d1656181362c4d2e7eabba42af6bddd
Mohammed Rabil <rabil@techie.com>
2023-09-25 18:06:39 +0530

    ignore cors middleware for `/render.ts` route
    As the RenderTSHandler sets all headers from JioTV ts link, the middleware is unneccessary

--- internals/middleware/cors.go
+++ internals/middleware/cors.go
@@ -6,6 +6,11 @@ import (
 
 func CORS() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		// if path is /render.ts then return c.Next()
+		if c.Path() == "/render.ts" {
+			return c.Next()
+		}
+
 		c.Set("Access-Control-Allow-Origin", "*")
 		c.Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
 
