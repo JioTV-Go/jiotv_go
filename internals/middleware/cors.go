a4061504a8296ec0d37b1a4bd0670d66656fdde7
Mohammed Rabil <rabil@techie.com>
2023-09-30 20:44:46 +0530

    Fix: cors pointer error for pass-through routes

--- internals/middleware/cors.go
+++ internals/middleware/cors.go
@@ -1,16 +1,20 @@
 package middleware
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func CORS() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		// if path is /render.ts then return c.Next()
-		if c.Path() == "/render.ts" {
-			return c.Next()
+		// ignore direct pass-through routes (proxy requests through server)
+		whitelist := []string{"/render.ts", "/jtvimage"}
+		for _, path := range whitelist {
+			if strings.Contains(c.Path(), path) {
+				return c.Next()
+			}
 		}
-
 		c.Set("Access-Control-Allow-Origin", "*")
 		c.Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
 
