b00020e719ab880263586bbed67e4c5445b94d48
Mohammed Rabil <rabil@techie.com>
2024-01-22 14:28:48 +0530

    Add example configuration files and update JioTVConfig struct
    This commit adds example configuration files for JioTV Go and updates the JioTVConfig struct in the internal/config package to include new fields corresponding to the added configuration options.

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,23 +10,23 @@ import (
 
 type JioTVConfig struct {
 	// Enable Or Disable EPG Generation. Default: false
-	EPG bool `yaml:"epg" env:"JIOTV_EPG"`
+	EPG bool `yaml:"epg" env:"JIOTV_EPG" json:"epg" toml:"epg"`
 	// Enable Or Disable Debug Mode. Default: false
-	Debug bool `yaml:"debug" env:"JIOTV_DEBUG"`
+	Debug bool `yaml:"debug" env:"JIOTV_DEBUG" json:"debug" toml:"debug"`
 	// Enable Or Disable TS Handler. While TS Handler is enabled, the server will serve the TS files directly from JioTV API. Default: false
-	DisableTSHandler bool `yaml:"disable_ts_handler" env:"JIOTV_DISABLE_TS_HANDLER"`
+	DisableTSHandler bool `yaml:"disable_ts_handler" env:"JIOTV_DISABLE_TS_HANDLER" json:"disable_ts_handler" toml:"disable_ts_handler"`
 	// Enable Or Disable Logout feature. Default: true
-	DisableLogout bool `yaml:"disable_logout" env:"JIOTV_DISABLE_LOGOUT"`
+	DisableLogout bool `yaml:"disable_logout" env:"JIOTV_DISABLE_LOGOUT" json:"disable_logout" toml:"disable_logout"`
 	// Enable Or Disable DRM. As DRM is not supported by most of the players, it is disabled by default. Default: false
-	DRM bool `yaml:"drm" env:"JIOTV_DRM"`
+	DRM bool `yaml:"drm" env:"JIOTV_DRM" json:"drm" toml:"drm"`
 	// Title of the webpage. Default: JioTV Go
-	Title string `yaml:"title" env:"JIOTV_TITLE"`
+	Title string `yaml:"title" env:"JIOTV_TITLE" json:"title" toml:"title"`
 	// Enable Or Disable URL Encryption. URL Encryption prevents hackers from injecting URLs into the server. Default: true
-	DisableURLEncryption bool `yaml:"disable_url_encryption" env:"JIOTV_DISABLE_URL_ENCRYPTION"`
+	DisableURLEncryption bool `yaml:"disable_url_encryption" env:"JIOTV_DISABLE_URL_ENCRYPTION" json:"disable_url_encryption" toml:"disable_url_encryption"`
 	// Path to the credentials file. Default: credentials.json
-	CredentialsPath string `yaml:"credentials_path" env:"JIOTV_CREDENTIALS_PATH"`
+	CredentialsPath string `yaml:"credentials_path" env:"JIOTV_CREDENTIALS_PATH" json:"credentials_path" toml:"credentials_path"`
 	// Proxy URL. Proxy is useful to bypass geo-restrictions and ip-restrictions for JioTV API. Default: ""
-	Proxy string `yaml:"proxy" env:"JIOTV_PROXY"`
+	Proxy string `yaml:"proxy" env:"JIOTV_PROXY" json:"proxy" toml:"proxy"`
 }
 
 // Global config variable
