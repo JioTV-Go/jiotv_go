88f04000266390c4bb0fbdc49850a889d8f0ff1d
Mohammed Rabil <rabil@techie.com>
2024-01-23 23:49:50 +0530

    Refactor architecture detection and error handling in init_script.ps1

--- init_script.ps1
+++ init_script.ps1
@@ -1,32 +1,39 @@
-# Identify processor architecture
-$architecture = (Get-WmiObject Win32_Processor).Architecture
-switch ($architecture) {
-    9 {
-        $arch = "arm64"
-        break
+try {
+    # Identify processor architecture
+    $architecture = (Get-WmiObject Win32_Processor).Architecture
+    switch ($architecture) {
+        9 {
+            $arch = "arm64"
+            break
+        }
+        0 {
+            $arch = "386"
+            break
+        }
+        5 {
+            $arch = "x86_64"
+            break
+        }
+        default {
+            throw "Unsupported architecture: $architecture"
+        }
     }
-    0 {
-        $arch = "386"
-        break
-    }
-    5 {
-        $arch = "x86_64"
-        break
-    }
-    default {
-        Write-Host "Unsupported architecture: $architecture"
-        exit 1
-    }
+
+    Write-Host "Detected architecture: $arch"
+
+    # Fetch the latest binary
+    $binaryUrl = "https://api.github.com/repos/rabilrbl/jiotv_go/releases/latest/download/jiotv_go-windows-$arch.exe"
+    Write-Host "Fetching the latest binary from $binaryUrl"
+    Invoke-WebRequest -Uri $binaryUrl -OutFile jiotv_go.exe -UseBasicParsing
+
+    # Add the directory to PATH
+    $binaryPath = Convert-Path ".\"
+    [Environment]::SetEnvironmentVariable("Path", "$($env:Path);$binaryPath", [EnvironmentVariableTarget]::Machine)
+
+    # Inform the user
+    Write-Host "JioTV Go has successfully downloaded and added to PATH. Start by running jiotv_go help"
 }
-
-# Fetch the latest binary
-$binaryUrl = "https://api.github.com/repos/rabilrbl/jiotv_go/releases/latest/download/jiotv_go-windows-$arch.exe"
-Write-Host "Fetching the latest binary from $binaryUrl"
-Invoke-WebRequest -Uri $binaryUrl -OutFile jiotv_go.exe -UseBasicParsing
-
-# Add the directory to PATH
-$binaryPath = Convert-Path ".\"
-[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$binaryPath", [EnvironmentVariableTarget]::Machine)
-
-# Inform the user
-Write-Host "JioTV Go has successfully downloaded and added to PATH. Start by running jiotv_go help"
+catch {
+    Write-Host "Error: $_"
+    exit 1
+}
