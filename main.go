48ef7120f935823612d0aae5b455458de155e768
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-24 09:17:31 +0000

    Bump version to v3.0.5

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		Name:      "JioTV Go",
 		Usage:     "Stream JioTV on any device",
 		HelpName:  "jiotv_go",
-		Version:  "v3.0.4",
+		Version:  "v3.0.5",
 		Copyright: "© JioTV Go by Mohammed Rabil (https://github.com/rabilrbl/jiotv_go)",
 		Compiled:  time.Now(),
 		Suggest:   true,
