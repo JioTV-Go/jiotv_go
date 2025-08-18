560f1c33af8ffbbde8964756ca3094f4e6cdc395
rabilrbl <mail@rabil.me>
2025-08-18 21:29:47 +0530

    fix: Handle case where no semver tag is found in version increment script

--- scripts/increment-version.sh
+++ scripts/increment-version.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # Get the latest tag that looks like a semver tag.
-tag=$(git describe --tags --match "v[0-9]*.[0-9]*.[0-9]*" --abbrev=0)
+tag=$(git describe --tags --match "v[0-9]*.[0-9]*.[0-9]*" --abbrev=0 || true)
 echo "Latest tag: $tag"
-if [[ $tag == "" ]]; then
+if [[ -z "$tag" || "$tag" == "true" ]]; then
     echo "No semver tag found, use 0.0.0"
     tag="v0.0.0"
 fi
