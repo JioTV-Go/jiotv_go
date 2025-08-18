83bc209fe67a028eb061c6bd2fcc767e5b687120
rabilrbl <mail@rabil.me>
2025-08-18 21:34:10 +0530

    refactor: Simplify architecture detection in build script

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -5,38 +5,39 @@ mkdir -p bin
 allowed_archs="amd64 arm arm64 386"
 echo "Building binaries for allowed architectures: $allowed_archs"
 for var in $(go tool dist list); do
+    IFS='/' read -r os arch <<< "$var"
     # skip disallowed archs
-    if [[ ! $allowed_archs =~ "$(cut -d '/' -f 2 <<<$var)" ]]; then
+    if [[ ! " $allowed_archs " =~ " $arch " ]]; then
         echo "Skipping: $var"
         continue
     fi
     # skip arm for windows
-    if [[ "$(cut -d '/' -f 1 <<<$var)" == "windows" && "$(cut -d '/' -f 2 <<<$var)" == "arm" ]]; then
+    if [[ "$os" == "windows" && "$arch" == "arm" ]]; then
         echo "Skipping: $var (windows/arm)"
         continue
     fi
     
-    file_name="jiotv_go-$(cut -d '/' -f 1 <<< $var)-$(cut -d '/' -f 2 <<< $var)"
-    case "$(cut -d '/' -f 1 <<< $var)" in
+    file_name="jiotv_go-${os}-${arch}"
+    case "$os" in
         "windows")
             echo "Building $var"
-            CGO_ENABLED=0 GOOS="$(cut -d '/' -f 1 <<< $var)" GOARCH="$(cut -d '/' -f 2 <<< $var)" go build -o bin/"$file_name.exe" -trimpath -ldflags="-s -w" . &
+            CGO_ENABLED=0 GOOS="$os" GOARCH="$arch" go build -o "bin/${file_name}.exe" -trimpath -ldflags="-s -w" . &
         ;;
         "linux" | "darwin")
             echo "Building $var"
-            CGO_ENABLED=0 GOOS="$(cut -d '/' -f 1 <<< $var)" GOARCH="$(cut -d '/' -f 2 <<< $var)" go build -o bin/"$file_name" -trimpath -ldflags="-s -w" . &
+            CGO_ENABLED=0 GOOS="$os" GOARCH="$arch" go build -o "bin/${file_name}" -trimpath -ldflags="-s -w" . &
         ;;
         "android")
             echo "Building $var"
-            case "$(cut -d '/' -f 2 <<<$var)" in
+            case "$arch" in
                 "arm")
-                    CGO_ENABLED=1 GOOS="$(cut -d '/' -f 1 <<<$var)" GOARCH="$(cut -d '/' -f 2 <<<$var)" CC="armv7a-linux-androideabi28-clang" CXX="armv7a-linux-androideabi28-clang++" go build -o bin/"jiotv_go-$(cut -d '/' -f 1 <<<$var)-$(cut -d '/' -f 2 <<<$var)" -trimpath -ldflags="-s -w" .
+                    CGO_ENABLED=1 GOOS="$os" GOARCH="$arch" CC="armv7a-linux-androideabi28-clang" CXX="armv7a-linux-androideabi28-clang++" go build -o "bin/jiotv_go-${os}-${arch}" -trimpath -ldflags="-s -w" .
                 ;;
                 "arm64")
-                    CGO_ENABLED=1 GOOS="$(cut -d '/' -f 1 <<<$var)" GOARCH="$(cut -d '/' -f 2 <<<$var)" CC="aarch64-linux-android32-clang" CXX="aarch64-linux-android32-clang++" go build -o bin/"jiotv_go-$(cut -d '/' -f 1 <<<$var)-$(cut -d '/' -f 2 <<<$var)" -trimpath -ldflags="-s -w" .
+                    CGO_ENABLED=1 GOOS="$os" GOARCH="$arch" CC="aarch64-linux-android32-clang" CXX="aarch64-linux-android32-clang++" go build -o "bin/jiotv_go-${os}-${arch}" -trimpath -ldflags="-s -w" .
                 ;;
                 "amd64")
-                    CGO_ENABLED=1 GOOS="$(cut -d '/' -f 1 <<<$var)" GOARCH="$(cut -d '/' -f 2 <<<$var)" CC="x86_64-linux-android32-clang" CXX="x86_64-linux-android32-clang++" go build -o bin/"jiotv_go-$(cut -d '/' -f 1 <<<$var)-$(cut -d '/' -f 2 <<<$var)" -trimpath -ldflags="-s -w" .
+                    CGO_ENABLED=1 GOOS="$os" GOARCH="$arch" CC="x86_64-linux-android32-clang" CXX="x86_64-linux-android32-clang++" go build -o "bin/jiotv_go-${os}-${arch}" -trimpath -ldflags="-s -w" .
                 ;;
                 *)
                     echo "Skipping: $var"
