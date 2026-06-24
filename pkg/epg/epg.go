49b2f783bcf70e4a03ad2d299ee55e31790e8dac
nithingowda <nithingowda856@gmail.com>
2026-06-24 12:27:06 +0530

    Update epg.go

--- pkg/epg/epg.go
+++ pkg/epg/epg.go
@@ -131,6 +131,7 @@ func genXML() ([]byte, error) {
 	// Create channels and programmes slices with initial capacity
 	var channels []Channel
 	var programmes []Programme
+    var programmeMutex sync.Mutex
 
 	// Define a worker function for fetching EPG data
 	fetchEPG := func(channel Channel, bar *progressbar.ProgressBar) {
@@ -144,7 +145,9 @@ func genXML() ([]byte, error) {
 			reqUrl := fmt.Sprintf(EPG_URL, offset, channel.ID)
 			req.SetRequestURI(reqUrl)
 
-			if err := client.Do(req, resp); err != nil {
+			if channel.ID == 1136 && offset == 0 {
+    utils.Log.Printf("EPG Response: %s", string(resp.Body()))
+}
 				// Handle error
 				utils.Log.Printf("Error fetching EPG for channel %d, offset %d: %v", channel.ID, offset, err)
 				continue
@@ -159,10 +162,34 @@ func genXML() ([]byte, error) {
 				continue
 			}
 
-			for _, programme := range epgResponse.EPG {
+			events := epgResponse.EPG
+
+if len(events) == 0 {
+    events = epgResponse.Result
+}
+
+if len(events) == 0 {
+    events = epgResponse.Data
+}
+
+for _, programme := range events {
 				startTime := formatTime(time.UnixMilli(programme.StartEpoch))
 				endTime := formatTime(time.UnixMilli(programme.EndEpoch))
-				programmes = append(programmes, NewProgramme(channel.ID, startTime, endTime, programme.Title, programme.Description, programme.ShowCategory, programme.Poster))
+				 programmeMutex.Lock()
+
+programmes = append(programmes,
+    NewProgramme(
+        channel.ID,
+        startTime,
+        endTime,
+        programme.Title,
+        programme.Description,
+        programme.ShowCategory,
+        programme.Poster,
+    ),
+)
+
+programmeMutex.Unlock()
 			}
 		}
 		bar.Add(1)
@@ -193,7 +220,7 @@ func genXML() ([]byte, error) {
 	}
 	utils.Log.Println("Fetched", len(channels), "channels")
 	// Use a worker pool to fetch EPG data concurrently
-	const numWorkers = 20 // Adjust the number of workers based on your needs
+	const numWorkers = 50 // Adjust the number of workers based on your needs
 	channelQueue := make(chan Channel, len(channels))
 	var wg sync.WaitGroup
 
